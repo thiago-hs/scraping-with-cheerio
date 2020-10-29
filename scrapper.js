2ae0360983a747afc7aa5630c714a24da08c9127
Thiago Henrique da Silva <thiago.h.silva.98@gmail.com>
2020-10-29 14:20:52 -0300

    basic scraping

---
+++ scrapper.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+console.log(process.argv);
+
+(async () => {
+
+    async function fetchHTML(url){
+
+        try {
+    
+            const { data } = await axios.get(url);
+        
+            return cheerio.load(data);
+    
+        } catch (error) {
+    
+            throw new Error('Invalid URL');
+            console.error(error);
+    
+        }
+    
+    }
+    
+    const [ ,,argURL ] = process.argv;
+    
+    const $ = await fetchHTML(argURL || "https://example.com")
+    
+    // full HTML
+    console.log(`Site HTML: ${$.html()} \n\n`)
+    
+    // specific content
+    console.log(`First h1 tag: ${$('h1').text()}`)
+    console.log(`First p tag: ${$('p').text()}`)
+
+})();
+
